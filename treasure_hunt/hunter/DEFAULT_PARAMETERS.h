16bab801dcc36ebcb42ab4d5107366e81182f0dc
pliat963 <43778199+pliat963@users.noreply.github.com>
2022-10-21 00:08:27 +0300

    Update DEFAULT_PARAMETERS.h

--- treasure_hunt/hunter/DEFAULT_PARAMETERS.h
+++ treasure_hunt/hunter/DEFAULT_PARAMETERS.h
@@ -1,12 +1,12 @@
 #ifndef _DEFAULT_PARAMETERS_H
 #define _DEFAULT_PARAMETERS_H
 
-#define TREASURES_NUMBER_D 3  // number of active treasures in the game. can be 1 or 2 in this case
+#define TREASURES_NUMBER_D 1  // number of active treasures in the game. can be 1 or 2 in this case
                               //OPTIONAL: if you add a treasure at SECTRETS.h, it can be  
                               //          between 1 and MAX_TREASURES_NUMBER
                         
 #define WITH_TIMER_D true // is there timer or not? true if there is, false if not 
-#define TIME_FOR_GAME_D 6 //time for the game in minutes. between 1 ang 59 (also if WITH_TIMER_D is false)
+#define TIME_FOR_GAME_D 4 //time for the game in minutes. between 1 ang 59 (also if WITH_TIMER_D is false)
 #define WITH_SOUND_D true //   // is there sound or not? true if there is, false if not
 
 
